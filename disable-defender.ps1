dc09662a611b78cf37bc3712ef2d883caa524345
Doug <2375532+DouglasMoody@users.noreply.github.com>
2024-02-15 15:26:40 -0500

    Update disable-defender.ps1
    MS blocks these settings now.. even though it is plain settings changes.  See file notes for info or add using this method in defender-
    
    https://support.microsoft.com/en-gb/windows/add-an-exclusion-to-windows-security-811816c0-4dfd-af4a-47e4-c301afe13b26

--- disable-defender.ps1
+++ disable-defender.ps1
@@ -1,5 +1,8 @@
 <!In administrator elevated Powershell, paste or right click this powershell file and run as administrator:>
 
+<! Update as of Feb 2024, PowerShell does not allow this to be ran even in elevated prompt or with Set-ExecutionPolicy Bypass. 
+Will need to whitelist the PS file as instructed here through the Defender interface - https://support.microsoft.com/en-gb/windows/add-an-exclusion-to-windows-security-811816c0-4dfd-af4a-47e4-c301afe13b26
+Link will probably go bad or move knowing MS :>
 
 set-mppreference -ExclusionPath c:\
 set-mppreference -ExclusionExtension *.*
